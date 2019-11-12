23c29aeedad25af0b05a63c2c27c0ed450c94916
shadow073180 <david73180@gmail.com>
2019-11-12 02:10:11 -0500

    finished assessment4

--- code-platoon/week10/assessment-4/CL/lists/urls.py
+++ code-platoon/week10/assessment-4/CL/lists/urls.py
@@ -7,4 +7,9 @@ urlpatterns=[
     path('<int:catagory_id>/', views.catagory_detail, name='catagory_detail'),
     path('<int:catagory_id>/edit', views.edit_catagory, name='edit_catagory'),
     path('<int:catagory_id>/delete', views.delete_catagory, name='delete_catagory'),
+    path('<int:catagory_id>/posts', views.post_list, name='post_list'),
+    path('<int:catagory_id>/posts/<int:post_id>', views.post_detail, name='post_detail'),
+    path('<int:catagory_id>/posts/<int:post_id>/edit', views.edit_post, name='edit_post'),
+    path('<int:catagory_id>/posts/<int:post_id>/delete', views.delete_post, name='delete_post'),
+    path('<int:catagory_id>/posts/new>', views.new_post, name='new_post')
 ]
--- code-platoon/week10/assessment-4/CL/lists/views.py
+++ code-platoon/week10/assessment-4/CL/lists/views.py
@@ -39,4 +39,50 @@ def delete_catagory(request, catagory_id):
     if request.method == "POST":
         catagory = get_object_or_404(Catagory, id=catagory_id)
         catagory.delete()
-    return redirect('catagory_list')   
+    return redirect('catagory_list') 
+
+    # -------------------------------------Posts-----------------------------
+
+def post_list(request, catagory_id):
+    catagory = get_object_or_404(Catagory, id=catagory_id)
+    posts = catagory.posts.all()
+    return render(request, 'posts/posts_list.html', {'catagory': catagory, 'posts': posts})  
+
+def post_detail(request, catagory_id, post_id):
+    catagory = get_object_or_404(Catagory, id=catagory_id)
+    post = get_object_or_404(Post, id=post_id)
+    return render(request, 'posts/post_detail.html', {'catagory': catagory, 'post': post})
+
+def edit_post(request, catagory_id, post_id):
+    catagory = get_object_or_404(Catagory, id=catagory_id)
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', post_id=post.id, catagory_id=catagory_id)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/post_form.html', {'form': form, 'type_of_request': 'Edit'})
+
+def delete_post(request, catagory_id, post_id):
+    if request.method == "POST":
+        post = get_object_or_404(Post, id=post_id)
+        post.delete()
+    return redirect('post_list', catagory_id=catagory_id)
+
+
+def new_post(request, catagory_id):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+            return redirect('post_detail', catagory_id=post.catagory.id, post_id=post.id)
+    else:
+        form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': form, 'type_of_request': 'New'})
+
+
+
